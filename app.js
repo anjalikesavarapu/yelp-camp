3ea4d3a31daddc8554d5dcb490bd3948fe9011b4
Anjali <anjalikesavarapu@gmail.com>
2023-11-08 20:17:50 +0530

    Added schema from review model and created basic review form to add reviews

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
+const Review = require('./models/review');
 const methodOverride = require('method-override');
 const catchAsync =require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const {campgroundSchema} = require('./schema')
 const ejsMate = require('ejs-mate');
+const { request } = require('express');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser : true,
@@ -77,6 +79,15 @@ app.delete('/campgrounds/:id', catchAsync(async(req, res) =>{
   const {id} = req.params;
   await Campground.findByIdAndDelete(id);
   res.redirect('/campgrounds');
+}))
+
+app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  const review = new Review(req.body.review);
+  campground.reviews.push(review);
+  await review.save();
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 }))
 
 app.all('*', (req, res, next) => {
