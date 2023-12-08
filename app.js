5825e8cc2a1fc069a6d489067907b51f84d8a02f
Anjali <anjalikesavarapu@gmail.com>
2023-12-08 12:20:49 +0530

    Added flash messages to know the updates

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const methodOverride = require('method-override');
+const session = require('express-session');
+const flash = require('connect-flash');
 const ExpressError = require('./utils/ExpressError');
 const ejsMate = require('ejs-mate');
-const { request } = require('express');
+
 
 const campgrounds = require('./routes/campground');
 const reviews = require('./routes/reviews');
@@ -24,9 +26,30 @@ const app =  express();
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
+
+
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname,'public')));
+const sessionConfig = {
+  secret:'thisshouldbeabettersecret',
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    httpOnly:true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  }
+}
+app.use(session(sessionConfig))
+app.use(flash());
+
+app.use((req, res, next) => {
+  res.locals.success = req.flash('success');
+  res.locals.error = req.flash('error');
+  next();
+})
+
 
 app.use('/campgrounds', campgrounds);
 app.use('/campgrounds/:id/reviews', reviews);
