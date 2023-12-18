eebddb7b300bcda21abbd5d1e47bd80f8c2459a3
Anjali <anjalikesavarapu@gmail.com>
2023-12-18 17:07:55 +0530

    Restructured the code by adding controllers

--- routes/user.js
+++ routes/user.js
@@ -1,53 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync')
-const User = require('../models/user');
 const passport = require('passport');
 const {storeReturnTo} = require('../middleware');
+const users = require('../controllers/users');
 
-router.get('/register', (req, res) =>{
-  res.render('users/register');
-})
+router.get('/register', users.renderRegister)
 
-router.post('/register', catchAsync(async(req, res, next) =>{
-  try{
-    const {username, email, password} = req.body;
-    const user = new User({username, email});
-    const registeredUser = await User.register(user, password);
-    req.login(registeredUser, err =>{
-      if(err) {
-        return next(err);
-      }
-      req.flash('success', "Welcome to the yelpCamp");
-      res.redirect('/campgrounds');
-    })
-  }catch(e){
-    req.flash('error', e.message);
-    res.redirect('register');
-  }  
-}));
+router.post('/register', catchAsync(users.register));
 
-router.get('/login', (req, res) =>{
-  res.render('users/login');
-})
+router.get('/login', users.renderLogin);
 
-router.post('/login',storeReturnTo,passport.authenticate('local', {failureFlash:true, failureRedirect:'/login',keepSessionInfo: true,}) ,(req, res) =>{
-  req.flash('success', 'Welccome back!');
-  const redirectTo = res.locals.returnTo || '/campgrounds'
-  res.redirect(redirectTo);
-})
+router.post('/login',storeReturnTo,passport.authenticate('local', {failureFlash:true, failureRedirect:'/login',keepSessionInfo: true,}) ,users.login)
 
-router.get('/logout', (req, res, next) =>{
-  // req.logout(function(err) {
-  //   if(err) {
-  //     return next(err);
-  //   }
-  //   req.flash('success', 'GoodBye');
-  //   res.redirect('/campgrounds');
-  // });
-  req.logout();
-  req.flash('success', 'GoodBye');
-  res.redirect('/campgrounds');
-})
+router.get('/logout', users.logout);
 
 module.exports = router;
--- seeds/index.js
+++ seeds/index.js
@@ -8,7 +8,6 @@ mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useUnifiedTopology:true
 })
 
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "connection error:"));
 db.once('open', () => {
